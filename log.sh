03f811866587fac44dd7ca32939e34767abf9cb1
atomu21263 <79088922+atomu21263@users.noreply.github.com>
2021-03-29 23:38:56 +0900

    Delete log.sh

--- log.sh
+++
@@ -1,4 +0,0 @@
-file=${1}.txt
-echo "<div id='top'>Thread : $(echo $file | sed -e 's/.txt//')</div>"
-echo "$(cat ./thread/$file | sed -z 's/\n/<br>/g' | sed -e 's/Name:/<\/table><br><table border="5" width="30%" bgcolor="f5f5f5"><tr  height="10"><td><div id='text_box'>Name:/g' -e 's/,Date:/  Date:/g' -e 's/,Text:/<\/div><\/td><\/tr><tr><td><div id='text_box'>/g')</table>"
-echo "<br><br>"
