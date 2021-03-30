2c746589d8b7f701f47560c8cb6f452fd4e88e09
atomu21263 <79088922+atomu21263@users.noreply.github.com>
2021-03-30 21:26:29 +0900

    Update index.php
    処理順変えたらきれいに動いたわ

--- bbs/index.php
+++ bbs/index.php
@@ -36,7 +36,7 @@
   <body>
     <!--サイドバー-->
         <!--heightは見出し*50 行*20-->
-    <table border="10" width="200" background="" cellpadding="0" cellspacing="10" align="right" id="sidebar">
+    <table border="10" width="200" bgcolor="faf0e6" cellpadding="0" cellspacing="10" align="right" id="sidebar">
       <caption>
         サイドバー
       </caption>
@@ -53,7 +53,7 @@
       </tr>
       <tr>
         <td>
-          任意(しても問題なし
+          任意
         </td>
       </tr>
         <tr height="50">
@@ -96,47 +96,26 @@
     </div>
     <!--メイン-->
     <?php
+      //表示限界
+      $maxline =500;
+      //表示するページ
       $page = $_GET['page'];
-      if ( $page != "") {
-        //page表示
-        $cmd = ('./log.sh '.$page);
-        echo shell_exec($cmd);
-      }else{
-        $url = ($_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
-        $cmd = ('./list.sh '.$url);
-        echo shell_exec($cmd);
-      }
-      $newpage = $_GET['newpage'];
-      $type = $_GET['type'];
-      if ( $newpage != "") {
-        //page生成
-        $cmd = ('./run.sh '.$newpage.' '.$type);
-        echo shell_exec($cmd);
-      }
-    ?>
-    <form action="" method="post">
-      <label for="name">Name:</label>
-        <input type="text" id="name" name="name">
-      <br>
-      <label for="message">Text:</label>
-        <input type="text" id="message" name="message"value="" size="30">
-      <br>
-      <input type="submit" name="send" value="送信する">
-    </form>
-    <?php
-      $maxline =50;
+      //行数の入手&整形
       $line = shell_exec('wc -l ./thread/'.$page.".txt | sed \"s|./thread/${page}.txt||\"");
       $line_text = $line + 1;
-      $page = $_GET['page'];
+      //リセット
       $name = "";
       $message = "";
       if (isset($_POST['send']) === true) {
+        //表示限界チェック
         if ($line >= $maxline) {
           echo "<div id='text_box'>メッセージが".$maxline."を超えました\n新しく立ててください</div>";
           exit;
         }
+        //メッセージのいろいろ
         $name = $_POST["name"];
         $message = $_POST["message"];
+        $message = str_replace(array("\r\n","\r","\n"), '<br>', $message);
         $date = date("Y/m/d H:i:s");
         $fp = fopen("./thread/".$page.".txt", "a");
         $line = str_replace(array("\r", "\n"), '', $line);
@@ -145,6 +124,33 @@
         header("Location: ./?page=".$page);
         exit;
       }
+      if ( $page != "") {
+        //page表示
+        $cmd = ('./log.sh '.$page);
+        echo shell_exec($cmd);
+      }else{
+        //page=がない時にlistの表示
+        $url = ($_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+        $cmd = ('./list.sh '.$url);
+        echo shell_exec($cmd);
+      }
+      //変数取得して実行
+      if (isset($_GET['newpage']) === true && isset($_GET['type']) === true) {
+        $newpage = $_GET['newpage'];
+        $type = $_GET['type'];
+        //page生成
+        $cmd = ('./run.sh '.$newpage.' '.$type);
+        echo shell_exec($cmd);
+      }
     ?>
+    <form action="" method="post">
+      <h3>Name:</h><br>
+        <input type="text" id="name" name="name">
+      <br>
+      <h3>Text:</h><br>
+        <textarea id="message" name="message" value="" style="width: 30%; height: 100px;" ></textarea>
+      <br>
+      <input type="submit" name="send" value="送信する">
+    </form>
   </body>
 </html>
