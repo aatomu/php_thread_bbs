3cf9284c6318e7593ffcfd1dc59d5109759df299
atomu21263 <79088922+atomu21263@users.noreply.github.com>
2021-03-31 00:30:24 +0900

    Update list.sh
    listを見やすく改善

--- bbs/list.sh
+++ bbs/list.sh
@@ -1,4 +1,17 @@
 url=${1}
 url=$(echo ${url} | sed -e "s|/|\/|" -e "s|/?.*|/|")
+file=$(ls ./thread/*.txt)
 echo "<div id='top'>スレッド一覧</div><br><br>"
-echo "<div id='text_box'>$(ls ./thread/*.txt | sed -e "s|^\./thread/|${url}?page=|" -e "s|.txt|<br>|")</div>"
+while :
+do
+  source=$(echo "${file}" | head -n 1 | sed -e "s|^\./thread/||" -e "s|.txt$||")
+  href=$(echo "${source}" | sed -e "s|^|<a href=${url}?page=|" -e "s|$|>Thread : ${source}</a>|")
+  link="<br><div id='text_box'>${href}</div>"
+  echo "${link}"
+  file=$(echo -e "${file}" | sed '1d')
+  if [ "${file}" = "" ]
+   then
+    echo "<br><br>"
+    exit
+  fi
+done
